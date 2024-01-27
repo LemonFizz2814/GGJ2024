5a4ede7027b2161203f599ead41be52460f783de
StarPatric <51011663+StarPatric@users.noreply.github.com>
2024-01-27 13:01:44 +1300

    Putting things in the cauldron does stuff
    - forgot to tag cauldron object
    - cauldron is a prefab

--- Assets/Scripts/CauldronEvents.cs
+++ Assets/Scripts/CauldronEvents.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class CauldronEvents : MonoBehaviour
 {
-    List<string> ObjectsInCauldron = new List<string>();
+    [Header("Cauldron Properties")]
+    public int MaxCauldronObjects;
+
+    private List<string> ObjectsInCauldron = new List<string>();
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Add Item to cauldron
         if (collision.gameObject.CompareTag("CauldronObject")) 
         {
             ObjectsInCauldron.Add(collision.gameObject.name);
             Debug.Log(string.Join(",", ObjectsInCauldron));
             Destroy(collision.gameObject);
+        }
+    }
+    private void Update()
+    {
+        // Only do things when cauldron has x number of items in it
+        if (ObjectsInCauldron.Count == MaxCauldronObjects) 
+        {
+            // Play Event
+            CauldronEvent();
+
+            // Remove whatever needs to be removed
+            RemoveCauldronItems();
+        }
+    }
+
+    // For now this just removes everything
+    private void RemoveCauldronItems()
+    {
+        ObjectsInCauldron.Clear();
+    }
+
+    private void CauldronEvent()
+    {
+        // Event 1
+        if (ObjectsInCauldron.Contains("Teapot") && ObjectsInCauldron.Contains("Glove"))
+        {
+            Debug.Log("EXPLOSION");
         }
     }
 }
